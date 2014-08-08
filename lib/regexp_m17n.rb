959df21c93a4882e1f1b4aa42054c5225b045456
Charles Dawson <charles.dwsn@gmail.com>
2014-08-08 10:33:41 -0700

    Add exception for dummy encodings

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,10 @@
 module RegexpM17N
   def self.non_empty?(str)
+    # Don't handle dummy encodings
+    if str.enc.dummy?
+      fail ArgumentError, 'argument must not be encoded with dummy encoding'
+    end
+    
     str.encode!('utf-8')
     str =~ /^.+$/
   end
