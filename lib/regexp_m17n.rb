f98546d5a50b91874d27700b72dc643c8332f5f9
Charles Dawson <charles.dwsn@gmail.com>
2014-08-10 00:27:41 -0400

    Step 1: Have no idea what you're doing Step 2: ... Step 3: Profit!

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,6 +1,11 @@
 module RegexpM17N
   def self.non_empty?(str)
-    r = Regexp.new('^.+$'.encode(str.encoding.name), Regexp::FIXEDENCODING)
+    if str.encoding.dummy?
+      str = str.force_encoding('utf-8')
+      r = Regexp.new('^.+$')
+    else
+      r = Regexp.new('^.+$'.encode(str.encoding.name))
+    end
 
     print "#{r.inspect} =~ #{str.encode('utf-8')} (#{r.encoding.name} with #{str.encoding.name}) "
 
