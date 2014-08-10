abcfae8693738808400084cdfffe62b1280f4e95
Charles Dawson <charles.dwsn@gmail.com>
2014-08-10 00:15:42 -0400

    Add test for exception when dummy encoding is passed

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,7 +1,7 @@
 module RegexpM17N
   def self.non_empty?(str)
     # Don't handle dummy encodings
-    if str.enc.dummy?
+    if str.encoding.dummy?
       fail ArgumentError, 'argument must not be encoded with dummy encoding'
     end
 
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -8,4 +8,12 @@ class RegexpTest < MiniTest::Unit::TestCase
       assert(RegexpM17N.non_empty?('.'.encode(enc))) unless enc.dummy?
     end
   end
+
+  def test_non_empty_string_with_dummies
+    Encoding.list.select(&:dummy?).each do |enc|
+      assert_raises ArgumentError do
+        RegexpM17N.non_empty?('.'.force_encoding(enc))
+      end
+    end
+  end
 end
